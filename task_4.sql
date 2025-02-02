7012cfb1078e60202d455d82edb1ec15dd9c1042
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-02-02 09:26:10 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,18 +1,13 @@
--- task_4.sql
-
--- Connect to the MySQL server and select the alx_book_store database
--- The database name is passed as an argument to the mysql command
-
--- Query to retrieve the full description of the books table
-SELECT 
+-- Print the full description of the books table
+SELECT
     COLUMN_NAME AS 'Field',
     COLUMN_TYPE AS 'Type',
     IS_NULLABLE AS 'Null',
     COLUMN_KEY AS 'Key',
     COLUMN_DEFAULT AS 'Default',
     EXTRA AS 'Extra'
-FROM 
-    information_schema.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
     AND TABLE_NAME = 'books';
