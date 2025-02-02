6e0a5ee7925b1a1b36a4f04aa0156cb72a9d1a41
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-02-02 09:28:56 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,3 @@
--- Print the full description of the books table
-SELECT
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_SCHEMA = 'alx_book_store'
-    AND TABLE_NAME = 'books';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
