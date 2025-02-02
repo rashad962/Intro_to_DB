4a9236e8a5a5f747f433047423c2d4304df8cce5
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-02-02 09:20:40 +0200

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,3 +1,3 @@
 -- List all tables in the alx_book_store database
 -- Note: The database is selected via the mysql command argument, so no USE statement is needed.
-SHOW TABLES;
+SHOW TABLES FROM alx_book_store;
