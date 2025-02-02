cfc18a64ef28b3fcb22a6f8c7e847f22c4ebb683
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-02-02 09:32:30 +0200

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,6 +1,1 @@
--- List all tables in the alx_book_store database
--- The database is selected via the mysql command argument, so no USE statement is needed.
 SHOW TABLES;
-SELECT table_name 
-FROM information_schema.tables 
-WHERE table_schema = 'alx_book_store';
